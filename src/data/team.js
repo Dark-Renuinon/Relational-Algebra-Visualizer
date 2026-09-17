d23b6f44565701fc3570495148894c40f063be06
Faisal-LR <zyrefw@gmail.com>
2026-09-17 23:09:13 +0530

    Change photo properties for team members
    Updated photo URLs for team members to use BASE_URL.

--- src/data/team.js
+++ src/data/team.js
@@ -1,6 +1,6 @@
 export const TEAM_MEMBERS = [
-  { slot: 'Member 01', name: 'Mohammed Faisal', registerNumber: '25BCE5777', photo: '' },
-  { slot: 'Member 02', name: 'Nandhan Arulselvan', registerNumber: '25BCE5752', photo: '' },
+  { slot: 'Member 01', name: 'Mohammed Faisal', registerNumber: '25BCE5777', photo: `${import.meta.env.BASE_URL}faisal.png` },
+  { slot: 'Member 02', name: 'Nandhan Arulselvan', registerNumber: '25BCE5752', photo: `${import.meta.env.BASE_URL}nandhan.png`},
 ];
 
 export const PROJECT_GUIDE = {
